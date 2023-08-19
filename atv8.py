c64cdd16b6d35b6a1a3cd0801ed6ebc39dcc979c
Felps06 <felipeperes8014@gmail.com>
2023-08-18 22:17:10 -0300

    v1

---
+++ atv8.py
@@ -0,0 +1,10 @@
+dados_clientes ={
+    "Nome": "Felipe",
+    "Endereço": "Rua ABC , 123",
+    "Telefone": "96092923"
+}
+
+print (dados_clientes["Telefone"]) # pra mudar o resultado so alterar esta linha 
+
+dados_clientes["cidade"] = "Godoy"
+print (dados_clientes)
